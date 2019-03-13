fff869bd633ae723a9d9184158c6c17fce335800
Gavin <gavinjboyd@gmail.com>
2019-03-13 10:32:49 +0900

    added basic test script, working

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,39 @@
-import{OutputProfile} from "./output-profile";
+//import{OutputProfile} from "./output-profile";
 import * as _ from "lodash";
 
-
 type Platform = "android" | "ios";
+type Orientation = "portrait" | "landscape";
+type Dimension= number;
+
+interface Dimensions {
+    height:Dimension,
+    width:Dimension,
+    orientation:Orientation
+}
 
 interface SizeProfile {
     size1:number,
     size2:number,
-    platform:Platform,
     //This will allow creation of new keys
-    [key:string]:any,
+    //[key:string]:any,
 }
+interface OutputProfile{
+    dimensions:Dimensions,
+    platform:Platform
+}
+let testInputProfile:SizeProfile={size1:100,size2:200};
 
-let defaultProfile =new OutputProfile();
-defaultProfile.height=200;
-defaultProfile.width=100;
-console.log(defaultProfile);
+console.log(getInputDimensions(testInputProfile));
 
+function getInputDimensions(inputProfile:SizeProfile):Dimensions{
+    //Give input dimensions and return dimensions with correct size and orientation
+    var dimensions: Dimensions;
 
+    if(inputProfile.size1>=inputProfile.size2){
+        dimensions= {width:inputProfile.size1,height:inputProfile.size2,orientation:"portrait"};
+    }else{
+        dimensions= {width:inputProfile.size1,height:inputProfile.size2,orientation:"landscape"}
+    }
+return dimensions;
+}
 
