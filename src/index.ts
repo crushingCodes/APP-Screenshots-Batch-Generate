75dc40020666cbcba64a02147c9a86a85db23051
Gavin <gavinjboyd@gmail.com>
2019-03-13 09:42:08 +0900

    added lodash typing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
 import{OutputProfile} from "./output-profile";
+import * as _ from "lodash";
+
+
+type Platform = "android" | "ios";
+
+interface SizeProfile {
+    size1:number,
+    size2:number,
+    platform:Platform,
+    //This will allow creation of new keys
+    [key:string]:any,
+}
 
 let defaultProfile =new OutputProfile();
 defaultProfile.height=200;
 defaultProfile.width=100;
 console.log(defaultProfile);
+
+
 
