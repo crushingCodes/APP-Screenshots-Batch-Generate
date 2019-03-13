f0f221c3273cc9c65772385107702e7652b00e29
Gavin <gavinjboyd@gmail.com>
2019-03-13 09:09:55 +0900

    simple console out working

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var output_profile_1 = require("./output-profile");
+var defaultProfile = new output_profile_1.OutputProfile();
+defaultProfile.height = 200;
+defaultProfile.width = 100;
+console.log(defaultProfile);
---
+++ src/output-profile.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var OutputProfile = /** @class */ (function () {
+    function OutputProfile() {
+    }
+    return OutputProfile;
+}());
+exports.OutputProfile = OutputProfile;
