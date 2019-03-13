d985764734e6044766e74d191c9f4b9a4cbe1b20
Gavin <gavinjboyd@gmail.com>
2019-03-13 09:59:06 +0900

    add output dir to tsconfig

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var output_profile_1 = require("./output-profile");
-var defaultProfile = new output_profile_1.OutputProfile();
-defaultProfile.height = 200;
-defaultProfile.width = 100;
-console.log(defaultProfile);
--- src/output-profile.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var OutputProfile = /** @class */ (function () {
-    function OutputProfile() {
-    }
-    return OutputProfile;
-}());
-exports.OutputProfile = OutputProfile;
